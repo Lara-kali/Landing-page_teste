8d869c1a5ead7bff2e16f9d5b20577c19b49daa1
pedro <larabianchi.code@gmail.com>
2023-08-18 11:27:16 -0300

    initegração do arquivo json com o arquivo js

--- src/js/main.js
+++ src/js/main.js
@@ -1,46 +1,27 @@
 $(document).ready(() => {
-    // SECTION HEADER
-    const navBar = [
-        {
-            title: "landing page",
-        },
-        {
-            link: ["home", "show more", "intro"],
-        },
-        {
-            cadastroBtn: ["login", "signup"],
-        },
-    ];
+fetch('src/js/data_json/dados.json')
+    .then(response => response.json())
+    .then(data => {
+            // SECTION HEADER
+    const nav_bar_data = data[0];
 
     $('<header>').addClass('section-header').append(
-        $('<section>').addClass('title-header').text(navBar[0]['title']),
+        $('<section>').addClass('title-header').text(nav_bar_data[0]['title']),
         $('<ul>').append(
-            $.map(navBar[1]['link'], function (i) {
+            $.map(nav_bar_data[1]['link'], function (i) {
                 return $('<li>').addClass('ul-li-header').text(i)
             })
         ),
         $('<section>').addClass('container-btn').append(
-            $.map(navBar[2]['cadastroBtn'], function (i) {
+            $.map(nav_bar_data[2]['cadastroBtn'], function (i) {
                 return $('<button>').addClass('btn btn-5').text(i)
             })
         )
     ).appendTo('body');
 
     // SECTION BANNER
-    const banner = [
-        {
-            title: "O Futuro no Seu Pulso: Descubra o Relógio Tecnológico Definitivo!",
-        },
-        {
-            placeholder: "O que está procurando?"
-        },
-        {
-            btn_action: "pesquisar"
-        },
-        {
-            img_banner: ["src/Imagens/Banner/img-relogio1.png", "relogio tecnologico - banner"],
-        },
-    ];
+
+    const banner_data = data[1]
 
     $("<section>")
         .addClass("section-banner")
@@ -48,34 +29,20 @@ $(document).ready(() => {
             $("<section>")
                 .addClass("section-text")
                 .append(
-                    $("<div>").addClass('title-banner').append(banner[0]["title"]),
+                    $("<div>").addClass('title-banner').append(banner_data[0]["title"]),
                     $('<div>').addClass('content-input').append(
-                        $('<input>').addClass('input-banner').attr('placeholder', banner[1]['placeholder']), // Adicionando o atributo placeholder
-                        $('<span>').text(banner[2]['btn_action'])
+                        $('<input>').addClass('input-banner').attr('placeholder', banner_data[1]['placeholder']), // Adicionando o atributo placeholder
+                        $('<span>').text(banner_data[2]['btn_action'])
                     )
                 ),
 
             $("<section>")
                 .addClass("section-img")
-                .append($("<img>").attr("src", banner[3]["img_banner"][0]))
+                .append($("<img>").attr("src", banner_data[3]["img_banner"][0]))
         ).appendTo("body");
 
     // SECTION ABOUT US
-    const about = [
-        {
-            title: "Sobre Nós - A Tecnologia em Cada Segundo",
-        },
-        {
-            paragrafo: [
-                "Bem-vindo à [Nome da Empresa], onde a alta tecnologia encontra a tradição horológica. Somos uma equipe apaixonada por relógios de alta tecnologia, dedicados a oferecer o que há de mais avançado e sofisticado no mundo dos relógios.",
-                "Nossa expertise reside em criar relógios que transcendem a simples medição do tempo. Desenvolvemos relógios inteligentes, conectados e repletos de funcionalidades inovadoras.",
-                "A busca pela inovação está no cerne de tudo o que fazemos. Cada novo modelo é fruto de pesquisa avançada e criatividade incansável, garantindo que nossos clientes desfrutem de recursos únicos e experiências que superam todas as expectativas.",
-            ],
-        },
-        {
-            btn_CTA: "Venha nos conhecer",
-        },
-    ];
+    const about_data = data[2]
 
     $("<section>")
         .addClass("section-about")
@@ -83,32 +50,21 @@ $(document).ready(() => {
             $("<section>")
                 .addClass("section-text")
                 .append(
-                    $("<div>").text(about[0]["title"]),
-                    $.map(about[1]["paragrafo"], function (i) {
+                    $("<div>").text(about_data[0]["title"]),
+                    $.map(about_data[1]["paragrafo"], function (i) {
                         return $("<p>").text(i);
                     }),
-                    $("<button>").text(about[2]["btn_CTA"])
+                    $("<button>").text(about_data[2]["btn_CTA"])
                 )
         ).appendTo("body");
 
     // SECTION MAIN
-    const carrousel = [
-        {
-            title: "O Futuro no Seu Pulso: Conheça Nossos Relógios Inteligentes",
-        },
-        {
-            collection_img: [
-                "src/Imagens/Slider/img-relogio2.png",
-                "src/Imagens/Slider/img-relogio3.png",
-                "src/Imagens/Slider/img-relogio4.png",
-            ],
-        },
-    ];
+    const carousel_data = data[3]
 
     $("<main>")
         .append(
             $("<section>").append(
-                $("<div>").addClass("title-main").text(carrousel[0]["title"])
+                $("<div>").addClass("title-main").text(carousel_data[0]["title"])
             ),
             $("<section>")
                 .addClass("splide")
@@ -120,7 +76,7 @@ $(document).ready(() => {
                             $("<ul>")
                                 .addClass("splide__list")
                                 .append(
-                                    $.map(carrousel[1]["collection_img"], function (i) {
+                                    $.map(carousel_data[1]["collection_img"], function (i) {
                                         return $("<li>")
                                             .addClass("splide__slide")
                                             .append($("<img>").attr("src", i));
@@ -136,45 +92,7 @@ $(document).ready(() => {
     }).mount();
 
     // SECTION PRODUTOS
-    const product = [
-        {
-            collection_card: [
-                {
-                    title_card: "AstraTime",
-                    subtitle_card: "Celestial Chrono",
-                    imagem: "src/Imagens/img_dos_produtos/RelogioP.1.png",
-                },
-                {
-                    title_card: "ChronoTech",
-                    subtitle_card: "Nexus Timekeeper",
-                    imagem: "src/Imagens/img_dos_produtos/RelogioP.2.png",
-                },
-                {
-                    title_card: "LuminaWatches",
-                    subtitle_card: "Aurora Vanguard",
-                    imagem: "src/Imagens/img_dos_produtos/RelogioP.3.png",
-                },
-                {
-                    title_card: "TemporalX",
-                    subtitle_card: "Eon Navigator",
-                    imagem: "src/Imagens/img_dos_produtos/RelogioP.4.png",
-                },
-                {
-                    title_card: "HorizonTimepieces",
-                    subtitle_card: "Lumina Horizon",
-                    imagem: "src/Imagens/img_dos_produtos/RelogioP.5.png",
-                },
-                {
-                    title_card: "AstralWatches",
-                    subtitle_card: "Stellar Chronograph",
-                    imagem: "src/Imagens/img_dos_produtos/RelogioP.6.png",
-                },
-            ],
-        },
-        {
-            btn_cta: "Ver mais",
-        },
-    ];
+    const produto_data = data[4]
 
     $("<section>")
         .addClass("section-card")
@@ -183,7 +101,7 @@ $(document).ready(() => {
             $("<section>")
                 .addClass("card-container")
                 .append(
-                    $.map(product[0]["collection_card"], function (i) {
+                    $.map(produto_data[0]["collection_card"], function (i) {
                         return $("<div>")
                             .addClass("card")
                             .append(
@@ -206,50 +124,18 @@ $(document).ready(() => {
             $("<section>")
                 .addClass("section-footer")
                 .append(
-                    $("<button>").addClass("btn-card btn-one").text(product[1]["btn_cta"])
+                    $("<button>").addClass("btn-card btn-one").text(produto_data[1]["btn_cta"])
                 )
         )
         .appendTo("body");
 
     // SECTION DEPOIMENTOS
-    const depoimentos = [
-        {
-            title_deploi: "depoimentos de clientes satisfeitos"
-        },
-        {
-            collection_deploi: [
-                {
-                    image: "src/Imagens/customers/JOHN_ANDERSON.jpg",
-                    name_person: "John Anderson",
-                    profission: "Profissional de Marketing Digital",
-                    deploi: "Os relógios desta marca são mais do que meros acessórios. Eles são verdadeiras obras de arte que me ajudam a manter o ritmo no mundo agitado do marketing digital. Estilo e funcionalidade em um só relógio."
-                },
-                {
-                    image: "src/Imagens/customers/EMILY_BENNETT.jpg",
-                    name_person: "Emily Bennett",
-                    profission: "Chef Confeiteira",
-                    deploi: "Na confeitaria, a precisão é a chave para criar obras-primas doces. Estes relógios me mantêm no tempo exato para criar sobremesas perfeitas e belas, assim como eles."
-                },
-                {
-                    image: "src/Imagens/customers/MICHAEL_TURNER.jpg",
-                    name_person: "Michael Turner",
-                    profission: "Atleta Profissional de Triatlo",
-                    deploi: "Cruzar a linha de chegada em triatlos exige disciplina e resistência. Estes relógios são mais do que um contador de tempo; eles são parceiros confiáveis que me ajudam a superar meus limites."
-                },
-                {
-                    image: "src/Imagens/customers/ISABELLA_RAMIREZ.jpg",
-                    name_person: "Isabella Ramirez",
-                    profission: "Artista Visual",
-                    deploi: "Assim como minhas obras de arte, esses relógios são uma expressão de estilo e originalidade. Eles me inspiram a criar e a manter um ritmo criativo, independentemente do momento."
-                }
-            ]
-        }
-    ]
+    const depoimento_data = data[5]
 
     $('<section>').addClass('section-deploi').append(
-        $('<section>').addClass('title-section-deploi').text(depoimentos[0]['title_deploi']),
+        $('<section>').addClass('title-section-deploi').text(depoimento_data[0]['title_deploi']),
         $('<section>').addClass('content-deploi').append(
-            $.map(depoimentos[1]["collection_deploi"], function (i) {
+            $.map(depoimento_data[1]["collection_deploi"], function (i) {
                 return $('<div>').addClass('deploi').append(
                     $('<img>').attr('src', i['image']),
                     $('<section>').addClass('info').append(
@@ -263,36 +149,12 @@ $(document).ready(() => {
     ).appendTo('body');
 
     // SECTION FOOTER
-    const footer = [
-        {
-            titleFooter: "Title-footer"
-        },
-        {
-            contentLink: [
-                {
-                    li: "facebook",
-                    icon: "fa-brands fa-facebook"
-                },
-                {
-                    li: "github",
-                    icon: "fa-brands fa-github"
-                },
-                {
-                    li: "twitter",
-                    icon: "fa-brands fa-twitter"
-                },
-                {
-                    li: "instagram",
-                    icon: "fa-brands fa-instagram"
-                }
-            ]
-        }
-    ]
+    const footer_data = data[6]
 
     $('<footer>').addClass('footer').append(
-        $('<div>').addClass('title-footer').text(footer[0]['titleFooter']),
+        $('<div>').addClass('title-footer').text(footer_data[0]['titleFooter']),
         $('<ul>').addClass('content-link').append(
-            $.map(footer[1]['contentLink'], function (i) {
+            $.map(footer_data[1]['contentLink'], function (i) {
                 return $('<li>').addClass('item').append(
                     $('<div>').text(i['li']),
                     $('<i>').addClass(i['icon'])
@@ -300,4 +162,5 @@ $(document).ready(() => {
             })
         )
     ).appendTo('body');
+    })
 })
