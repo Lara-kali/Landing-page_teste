1e0b1beb3aeebd72c089f565edb1a57ccf611aa5
pedro <larabianchi.code@gmail.com>
2023-08-22 08:18:23 -0300

    adição dos arquivos js e json

--- src/js/main.js
+++ src/js/main.js
@@ -1,170 +1,170 @@
 $(document).ready(() => {
-fetch('src/js/data_json/dados.json')
-    .then(response => response.json())
-    .then(data => {
+    fetch('src/js/data_json/dados.json')
+        .then(response => response.json())
+        .then(data => {
 
-        const nav_bar_data = data[0];
+            const nav_bar_data = data[0];
 
-        const banner_data = data[1];
+            const banner_data = data[1];
 
             // SECTION HEADER
-        $('<section>')
-            .addClass('section_banner-and-navbar')
-            .css("background-image", 'url("src/Imagens/Banner/immmmm(2).jpg")')
-            .append(
-            $('<header>').addClass('section-header').append(
-                $('<section>').addClass('title-header').text(nav_bar_data[0]['title']),
-                $('<ul>').append(
-                    $.map(nav_bar_data[1]['link'], function (i) {
-                        return $('<li>').addClass('ul-li-header').text(i)
-                    })
-                ),
-                $('<section>').addClass('container-btn').append(
-                    $.map(nav_bar_data[2]['cadastroBtn'], function (i) {
-                        return $('<button>').addClass('btn btn-5').text(i)
-                    })
-                )
-            ),
-        
-            // SECTION BANNER
-                
+            $('<section>')
+                .addClass('section_banner-and-navbar')
+                .css("background-image", 'url("src/Imagens/Banner/immmmm(2).jpg")')
+                .append(
+                    $('<header>').addClass('section-header').append(
+                        $('<section>').addClass('title-header').text(nav_bar_data[0]['title']),
+                        $('<ul>').append(
+                            $.map(nav_bar_data[1]['link'], function (i) {
+                                return $('<li>').addClass('ul-li-header').text(i)
+                            })
+                        ),
+                        $('<section>').addClass('container-btn').append(
+                            $.map(nav_bar_data[2]['cadastroBtn'], function (i) {
+                                return $('<button>').addClass('btn btn-5').text(i)
+                            })
+                        )
+                    ),
+
+                    // SECTION BANNER
+
+                    $("<section>")
+                        .addClass("section-banner")
+                        .append(
+                            $("<section>")
+                                .addClass("section-text")
+                                .append(
+                                    $("<div>").addClass('title-banner').append(banner_data[0]["title"]),
+                                    $('<div>').addClass('content-input').append(
+                                        $('<input>').addClass('input-banner').attr('placeholder', banner_data[1]['placeholder']), // Adicionando o atributo placeholder
+                                        $('<button>').addClass('btn-4').text(banner_data[2]['btn_action'])
+                                    )
+                                )
+                        )
+                ).appendTo('body')
+
+            // SECTION ABOUT US
+            const about_data = data[2]
+
             $("<section>")
-                .addClass("section-banner")
+                .addClass("section-about")
                 .append(
                     $("<section>")
                         .addClass("section-text")
                         .append(
-                            $("<div>").addClass('title-banner').append(banner_data[0]["title"]),
-                            $('<div>').addClass('content-input').append(
-                                $('<input>').addClass('input-banner').attr('placeholder', banner_data[1]['placeholder']), // Adicionando o atributo placeholder
-                                $('<button>').addClass('btn-4').text(banner_data[2]['btn_action'])
-                            )
+                            $("<div>").text(about_data[0]["title"]),
+                            $.map(about_data[1]["paragrafo"], function (i) {
+                                return $("<p>").text(i);
+                            }),
+                            $("<button>").text(about_data[2]["btn_CTA"])
                         )
-                )
-        ).appendTo('body')
+                ).appendTo("body");
 
-    // SECTION ABOUT US
-    const about_data = data[2]
+            // SECTION MAIN
+            const carousel_data = data[3]
 
-    $("<section>")
-        .addClass("section-about")
-        .append(
-            $("<section>")
-                .addClass("section-text")
+            $("<main>")
+                .css('background-image', 'url("src/Imagens/Banner/cool-background (8).png")')
                 .append(
-                    $("<div>").text(about_data[0]["title"]),
-                    $.map(about_data[1]["paragrafo"], function (i) {
-                        return $("<p>").text(i);
-                    }),
-                    $("<button>").text(about_data[2]["btn_CTA"])
-                )
-        ).appendTo("body");
-
-    // SECTION MAIN
-    const carousel_data = data[3]
-
-    $("<main>")
-        .css('background-image', 'url("src/Imagens/Banner/cool-background (8).png")')
-        .append(
-            $("<section>").append(
-                $("<div>").addClass("title-main").text(carousel_data[0]["title"])
-            ),
-            $("<section>")
-                .addClass("splide")
-                .attr("id", "image-carousel")
-                .append(
-                    $("<div>")
-                        .addClass("splide__track")
+                    $("<section>").append(
+                        $("<div>").addClass("title-main").text(carousel_data[0]["title"])
+                    ),
+                    $("<section>")
+                        .addClass("splide")
+                        .attr("id", "image-carousel")
                         .append(
-                            $("<ul>")
-                                .addClass("splide__list")
+                            $("<div>")
+                                .addClass("splide__track")
                                 .append(
-                                    $.map(carousel_data[1]["collection_img"], function (i) {
-                                        return $("<li>")
-                                            .addClass("splide__slide")
-                                            .append($("<img>").attr("src", i));
-                                    })
+                                    $("<ul>")
+                                        .addClass("splide__list")
+                                        .append(
+                                            $.map(carousel_data[1]["collection_img"], function (i) {
+                                                return $("<li>")
+                                                    .addClass("splide__slide")
+                                                    .append($("<img>").attr("src", i));
+                                            })
+                                        )
                                 )
                         )
                 )
-        )
-        .appendTo("body");
+                .appendTo("body");
 
-    new Splide("#image-carousel", {
-        heightRatio: 0.5,
-    }).mount();
+            new Splide("#image-carousel", {
+                heightRatio: 0.5,
+            }).mount();
 
-    // SECTION PRODUTOS
-    const produto_data = data[4]
+            // SECTION PRODUTOS
+            const produto_data = data[4]
 
-    $("<section>")
-        .css("background-image", 'url("src/Imagens/Banner/cool-background (2).png")')
-        .addClass("section-card")
-        .append(
-            $("<section>").addClass("section-title").text("Confira nossos produtos"),
             $("<section>")
-                .addClass("card-container")
+                .css("background-image", 'url("src/Imagens/Banner/cool-background (2).png")')
+                .addClass("section-card")
                 .append(
-                    $.map(produto_data[0]["collection_card"], function (i) {
-                        return $("<div>")
-                            .addClass("card")
-                            .append(
-                                $("<div>")
-                                    .addClass("card-image")
+                    $("<section>").addClass("section-title").text("Confira nossos produtos"),
+                    $("<section>")
+                        .addClass("card-container")
+                        .append(
+                            $.map(produto_data[0]["collection_card"], function (i) {
+                                return $("<div>")
+                                    .addClass("card")
                                     .append(
-                                        $("<img>")
-                                            .attr("src", i["imagem"])
-                                            .addClass("image-responsive")
-                                    ),
-                                $("<div>")
-                                    .addClass("card-header")
-                                    .append(
-                                        $("<div>").addClass("card-title").text(i["title_card"]),
-                                        $('<div>').addClass('card-subtitle').text(i['subtitle_card'])
-                                    )
-                            );
+                                        $("<div>")
+                                            .addClass("card-image")
+                                            .append(
+                                                $("<img>")
+                                                    .attr("src", i["imagem"])
+                                                    .addClass("image-responsive")
+                                            ),
+                                        $("<div>")
+                                            .addClass("card-header")
+                                            .append(
+                                                $("<div>").addClass("card-title").text(i["title_card"]),
+                                                $('<div>').addClass('card-subtitle').text(i['subtitle_card'])
+                                            )
+                                    );
+                            })
+                        ),
+                    $("<section>")
+                        .addClass("section-footer")
+                        .append(
+                            $("<button>").addClass("btn-card btn-one").text(produto_data[1]["btn_cta"])
+                        )
+                )
+                .appendTo("body");
+
+            // SECTION DEPOIMENTOS
+            const depoimento_data = data[5]
+
+            $('<section>').addClass('section-deploi').append(
+                $('<section>').addClass('title-section-deploi').text(depoimento_data[0]['title_deploi']),
+                $('<section>').addClass('content-deploi').append(
+                    $.map(depoimento_data[1]["collection_deploi"], function (i) {
+                        return $('<div>').addClass('deploi').append(
+                            $('<img>').attr('src', i['image']),
+                            $('<section>').addClass('info').append(
+                                $('<div>').addClass('name').text(i['name_person']),
+                                $('<div>').addClass('profission').text(i['profission']),
+                                $('<div>').addClass('text').text(i['deploi'])
+                            ),
+                        )
                     })
-                ),
-            $("<section>")
-                .addClass("section-footer")
-                .append(
-                    $("<button>").addClass("btn-card btn-one").text(produto_data[1]["btn_cta"])
                 )
-        )
-        .appendTo("body");
+            ).appendTo('body');
 
-    // SECTION DEPOIMENTOS
-    const depoimento_data = data[5]
+            // SECTION FOOTER
+            const footer_data = data[6]
 
-    $('<section>').addClass('section-deploi').append(
-        $('<section>').addClass('title-section-deploi').text(depoimento_data[0]['title_deploi']),
-        $('<section>').addClass('content-deploi').append(
-            $.map(depoimento_data[1]["collection_deploi"], function (i) {
-                return $('<div>').addClass('deploi').append(
-                    $('<img>').attr('src', i['image']),
-                    $('<section>').addClass('info').append(
-                        $('<div>').addClass('name').text(i['name_person']),
-                        $('<div>').addClass('profission').text(i['profission']),
-                        $('<div>').addClass('text').text(i['deploi'])
-                    ),
+            $('<footer>').addClass('footer').append(
+                $('<div>').addClass('title-footer').text(footer_data[0]['titleFooter']),
+                $('<ul>').addClass('content-link').append(
+                    $.map(footer_data[1]['contentLink'], function (i) {
+                        return $('<li>').addClass('item').append(
+                            $('<div>').text(i['li']),
+                            $('<i>').addClass(i['icon'])
+                        )
+                    })
                 )
-            })
-        )
-    ).appendTo('body');
-
-    // SECTION FOOTER
-    const footer_data = data[6]
-
-    $('<footer>').addClass('footer').append(
-        $('<div>').addClass('title-footer').text(footer_data[0]['titleFooter']),
-        $('<ul>').addClass('content-link').append(
-            $.map(footer_data[1]['contentLink'], function (i) {
-                return $('<li>').addClass('item').append(
-                    $('<div>').text(i['li']),
-                    $('<i>').addClass(i['icon'])
-                )
-            })
-        )
-    ).appendTo('body');
-    })
+            ).appendTo('body');
+        })
 })
